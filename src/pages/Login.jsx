c7a22a251609996de0075a14359db60ba731c480
Umesh Chimane <umeshrc2002@gmail.com>
2024-08-22 22:19:01 +0530

    First Commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <main>
+        <h1 className='text-3xl font-bold underline'> Its Umesh</h1>
+    </main>
+  )
+}
+export default login
